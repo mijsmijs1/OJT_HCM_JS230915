a5e0e2bc06a94deb25ac814cbd36b2fd5d69e022
root <root@NHANYUKI>
2024-04-08 16:26:09 +0700

    create structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Outlet } from 'react-router-dom'
+
+import './layout.scss'
+
+export default function Layout() {
+    // Back to top
+    const handleBackToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+    return (
+        <div className="layout_container">
+            <div className="header_container">
+                <Header />
+            </div>
+            <div className='home_page_body'>
+                <div className='body_content'>
+                    <Outlet />
+                </div>
+            </div>
+            <Footer handleBackToTop={handleBackToTop} />
+        </div>
+    )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '@pages/home'
+
 export default function RouteSetup() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} >
+        </Route>
       </Routes>
     </BrowserRouter>
   )
