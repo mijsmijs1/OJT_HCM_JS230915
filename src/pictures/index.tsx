e339d87a5eb642daf991ee53b7b24fbac7463faa
MinhThy2k1 <minhthy031@gmail.com>
2024-04-09 01:37:22 +0700

    update/login user

--- src/pictures/index.tsx
+++ src/pictures/index.tsx
@@ -2,5 +2,7 @@
 export default {
     logo_RikkeiEdu: './images/logo/logo_RikkeiEdu.png',
     logo_facebook: './images/logo/logo_facebook.png',
-    logo_youtube: './images/logo/logo_youtube.png'
+    logo_youtube: './images/logo/logo_youtube.png',
+    logo_RikkeiEduV2: './images/logo/logo_RikkeiEdu_Color.png',
+    img_content: './images/logo/img_content.png',
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '@pages/home'
+import Login from "@components/authen/loginuser/LoginUser"
 
 export default function RouteSetup() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} >
+        </Route>
+        <Route path='/login' element={<Login />} >
         </Route>
       </Routes>
     </BrowserRouter>
