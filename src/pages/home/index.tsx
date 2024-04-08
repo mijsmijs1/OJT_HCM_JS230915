f5fbcaff37f7abe29f27bf9c51204b9005b49ab0
Your Name <nguyphuquy1@gmail.com>
2024-04-08 17:46:47 +0700

    feature/breadcrumb

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import Footer from "@/components/Footer/Footer";
+import Footer from "@/components/footer/Footer";
 import { Outlet } from 'react-router-dom'
 
 import './layout.scss'
