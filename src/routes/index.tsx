0ceab222388ed1e607a2c20d857913d80e5fce64
MinhThy2k1 <minhthy031@gmail.com>
2024-04-09 09:50:17 +0700

    fix/folder

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '@pages/home'
+import Login from "@/components/authen/loginUser/LoginUser"
+import Register from "@/components/authen/registerUser/registerUser"
+import LoginAdmin from "@/components/authen/loginAdmin/LoginAdmin"
 
 export default function RouteSetup() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} >
+        </Route>
+        <Route path='/login' element={<Login />} >
+        </Route>
+        <Route path='/register' element={<Register />} >
+        </Route>
+        <Route path='/admin' element={<LoginAdmin />} >
         </Route>
       </Routes>
     </BrowserRouter>
